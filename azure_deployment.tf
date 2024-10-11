7d4d80ef2967aeca4cbece84d8895d488c5b5c7f
Lucas Gauvain <lucasgauvain@gmail.com>
2024-10-11 10:32:56 +0200

    aks cluster works!

--- azure_deployment.tf
+++ azure_deployment.tf
@@ -1,25 +1,4 @@
 # Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.20"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
@@ -27,10 +6,10 @@ provider "azurerm" {
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
   name     = "my-aks-rg"
-  location = "East US"
+  location = "eastus"
 }
 
-# Create AKS cluster with 3 nodes
+# Create an AKS cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "my-aks-cluster"
   location            = azurerm_resource_group.rg.location
@@ -39,8 +18,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 3
-    vm_size    = "Standard_D2_v2"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
   }
 
   identity {
@@ -48,22 +27,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# Define a variable for PostgreSQL password
-variable "postgres_password" {
-  description = "The password for the PostgreSQL server administrator."
-  type        = string
-}
-
-# Generate a random string for uniqueness
-resource "random_string" "unique_id" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
-# Create Azure Database for PostgreSQL with a unique name
+# Create an Azure Database for PostgreSQL server
 resource "azurerm_postgresql_server" "postgres" {
-  name                = "my-postgres-${random_string.unique_id.result}"
+  name                = "postgres-server"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -74,13 +40,14 @@ resource "azurerm_postgresql_server" "postgres" {
   geo_redundant_backup_enabled = false
   auto_grow_enabled            = true
 
-  administrator_login          = "postgres"
+  administrator_login          = "psqladmin"
   administrator_login_password = var.postgres_password
   version                      = "11"
   ssl_enforcement_enabled      = true
 }
 
-resource "azurerm_postgresql_database" "mydatabase" {
+# Create a database
+resource "azurerm_postgresql_database" "database" {
   name                = "mydatabase"
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_postgresql_server.postgres.name
@@ -88,7 +55,16 @@ resource "azurerm_postgresql_database" "mydatabase" {
   collation           = "English_United States.1252"
 }
 
-# Configure the Kubernetes provider
+# Allow access to Azure services
+resource "azurerm_postgresql_firewall_rule" "azure_services" {
+  name                = "allow-azure-services"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_postgresql_server.postgres.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+# Kubernetes provider configuration
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
@@ -96,7 +72,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 }
 
-# Configure the Helm provider
+# Helm provider configuration
 provider "helm" {
   kubernetes {
     host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
@@ -106,10 +82,26 @@ provider "helm" {
   }
 }
 
-# Deploy the application with one replica per node
-resource "kubernetes_deployment" "fastapi" {
+# Create a Kubernetes secret for the database URL
+resource "kubernetes_secret" "database_url" {
   metadata {
-    name = "fastapi"
+    name = "database-url"
+  }
+
+  data = {
+    url = "postgresql://psqladmin%40${azurerm_postgresql_server.postgres.name}:${var.postgres_password}@${azurerm_postgresql_server.postgres.fqdn}:5432/mydatabase?sslmode=require"
+  }
+
+  depends_on = [
+    azurerm_postgresql_server.postgres,
+    azurerm_postgresql_database.database
+  ]
+}
+
+# Deploy custom webapp
+resource "kubernetes_deployment" "webapp" {
+  metadata {
+    name = "webapp"
   }
 
   spec {
@@ -117,82 +109,87 @@ resource "kubernetes_deployment" "fastapi" {
 
     selector {
       match_labels = {
-        app = "fastapi"
+        app = "webapp"
       }
     }
 
     template {
       metadata {
         labels = {
-          app = "fastapi"
+          app = "webapp"
         }
       }
 
       spec {
-        affinity {
-          pod_anti_affinity {
-            required_during_scheduling_ignored_during_execution {
-              label_selector {
-                match_expressions {
-                  key      = "app"
-                  operator = "In"
-                  values   = ["fastapi"]
-                }
-              }
-              topology_key = "kubernetes.io/hostname"
-            }
-          }
-        }
-
         container {
           image = "ghcr.io/phromaj/cloud-webapp-auto-deploy:latest"
-          name  = "fastapi"
+          name  = "webapp"
 
           env {
-            name  = "DATABASE_URL"
-            value = "postgresql://postgres:${var.postgres_password}@${azurerm_postgresql_server.postgres.fqdn}:5432/mydatabase?sslmode=require"
+            name = "DATABASE_URL"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.database_url.metadata[0].name
+                key  = "url"
+              }
+            }
+          }
+
+          port {
+            container_port = 8000
           }
         }
       }
     }
   }
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks,
+    kubernetes_secret.database_url
+  ]
 }
 
-# Expose FastAPI using a Kubernetes Service
-resource "kubernetes_service" "fastapi" {
+# Expose webapp service
+resource "kubernetes_service" "webapp" {
   metadata {
-    name = "fastapi"
+    name = "webapp"
   }
 
   spec {
     selector = {
-      app = "fastapi"
+      app = "webapp"
     }
 
     port {
       port        = 8000
       target_port = 8000
     }
+
+    type = "ClusterIP"
   }
 }
 
-# Install NGINX Ingress Controller using Helm
+# Install Nginx Ingress Controller using Helm
 resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  namespace  = "default"
+  version    = "4.7.1"
 
   set {
     name  = "controller.service.type"
     value = "LoadBalancer"
   }
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
 }
 
-# Create an Ingress resource for FastAPI
-resource "kubernetes_ingress_v1" "fastapi_ingress" {
+# Create Ingress resource
+resource "kubernetes_ingress_v1" "webapp_ingress" {
   metadata {
-    name = "fastapi-ingress"
+    name = "webapp-ingress"
     annotations = {
       "kubernetes.io/ingress.class" = "nginx"
     }
@@ -206,7 +203,7 @@ resource "kubernetes_ingress_v1" "fastapi_ingress" {
           path_type = "Prefix"
           backend {
             service {
-              name = kubernetes_service.fastapi.metadata[0].name
+              name = kubernetes_service.webapp.metadata[0].name
               port {
                 number = 8000
               }
@@ -216,4 +213,31 @@ resource "kubernetes_ingress_v1" "fastapi_ingress" {
       }
     }
   }
+
+  depends_on = [
+    helm_release.nginx_ingress
+  ]
+}
+
+# Output the external IP of the Nginx Ingress Controller
+data "kubernetes_service" "nginx_ingress" {
+  metadata {
+    name      = "nginx-ingress-ingress-nginx-controller"
+    namespace = "default"
+  }
+
+  depends_on = [
+    helm_release.nginx_ingress
+  ]
+}
+
+output "nginx_ingress_ip" {
+  value = data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].ip
+}
+
+# Variable declaration for PostgreSQL password
+variable "postgres_password" {
+  description = "Password for the PostgreSQL server"
+  type        = string
+  sensitive   = true
 }
