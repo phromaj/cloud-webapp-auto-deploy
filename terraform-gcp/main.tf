d0491221305b0db12460f7eb28528d6f81f7437d
NGUYEN Minh Kha <58908433+Nguyen-Minh-Kha@users.noreply.github.com>
2024-10-11 14:18:49 +0200

    Update main.tf

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -236,7 +236,250 @@ resource "kubernetes_service" "nginx" {
   }
 }
 
+# Helm provider configuration
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  }
+}
+
+# Create a ClusterRole for Prometheus
+resource "kubernetes_cluster_role" "prometheus" {
+  metadata {
+    name = "prometheus"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["nodes", "nodes/proxy", "services", "endpoints", "pods"]
+    verbs      = ["get", "list", "watch"]
+  }
+
+  rule {
+    api_groups = ["extensions", "networking.k8s.io"]
+    resources  = ["ingresses"]
+    verbs      = ["get", "list", "watch"]
+  }
+
+  rule {
+    non_resource_urls = ["/metrics"]
+    verbs             = ["get"]
+  }
+}
+
+# Create a ClusterRoleBinding for Prometheus
+resource "kubernetes_cluster_role_binding" "prometheus" {
+  metadata {
+    name = "prometheus"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.prometheus.metadata[0].name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = "prometheus"
+    namespace = "monitoring"
+  }
+}
+
+# Create a namespace for monitoring
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+# Deploy Prometheus using Helm
+# Deploy Prometheus using Helm
+resource "helm_release" "prometheus" {
+  name             = "prometheus"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus"
+  namespace        = kubernetes_namespace.monitoring.metadata[0].name
+  create_namespace = false
+
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccounts.server.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccounts.server.name"
+    value = "prometheus"
+  }
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "server.global.scrape_interval"
+    value = "15s"
+  }
+
+  set {
+    name  = "server.global.evaluation_interval"
+    value = "15s"
+  }
+
+  values = [
+    <<-EOT
+    serverFiles:
+      prometheus.yml:
+        scrape_configs:
+          - job_name: 'kubernetes-apiservers'
+            kubernetes_sd_configs:
+            - role: endpoints
+            scheme: https
+            tls_config:
+              ca_file: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+            bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
+            relabel_configs:
+            - source_labels: [__meta_kubernetes_namespace, __meta_kubernetes_service_name, __meta_kubernetes_endpoint_port_name]
+              action: keep
+              regex: default;kubernetes;https
+
+          - job_name: 'kubernetes-nodes'
+            scheme: https
+            tls_config:
+              ca_file: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+            bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
+            kubernetes_sd_configs:
+            - role: node
+            relabel_configs:
+            - action: labelmap
+              regex: __meta_kubernetes_node_label_(.+)
+            - target_label: __address__
+              replacement: kubernetes.default.svc:443
+            - source_labels: [__meta_kubernetes_node_name]
+              regex: (.+)
+              target_label: __metrics_path__
+              replacement: /api/v1/nodes/${1}/proxy/metrics
+
+          - job_name: 'kubernetes-pods'
+            kubernetes_sd_configs:
+            - role: pod
+            relabel_configs:
+            - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_scrape]
+              action: keep
+              regex: true
+            - source_labels: [__meta_kubernetes_pod_annotation_prometheus_io_path]
+              action: replace
+              target_label: __metrics_path__
+              regex: (.+)
+            - source_labels: [__address__, __meta_kubernetes_pod_annotation_prometheus_io_port]
+              action: replace
+              regex: ([^:]+)(?::\d+)?;(\d+)
+              replacement: $1:$2
+              target_label: __address__
+            - action: labelmap
+              regex: __meta_kubernetes_pod_label_(.+)
+            - source_labels: [__meta_kubernetes_namespace]
+              action: replace
+              target_label: kubernetes_namespace
+            - source_labels: [__meta_kubernetes_pod_name]
+              action: replace
+              target_label: kubernetes_pod_name
+
+          - job_name: 'kubernetes-cadvisor'
+            scheme: https
+            tls_config:
+              ca_file: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+            bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
+            kubernetes_sd_configs:
+            - role: node
+            relabel_configs:
+            - action: labelmap
+              regex: __meta_kubernetes_node_label_(.+)
+            - target_label: __address__
+              replacement: kubernetes.default.svc:443
+            - source_labels: [__meta_kubernetes_node_name]
+              regex: (.+)
+              target_label: __metrics_path__
+              replacement: /api/v1/nodes/${1}/proxy/metrics/cadvisor
+    EOT
+  ]
+
+  depends_on = [
+    google_container_cluster.primary,
+    kubernetes_cluster_role_binding.prometheus
+  ]
+}
+
+# Deploy Grafana using Helm
+resource "helm_release" "grafana" {
+  name             = "grafana"
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "grafana"
+  namespace        = kubernetes_namespace.monitoring.metadata[0].name
+  create_namespace = false
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "adminPassword"
+    value = "admin"  # You should change this to a more secure password
+  }
+
+  depends_on = [
+    google_container_cluster.primary,
+    helm_release.prometheus
+  ]
+}
+
 # Output the external IP of the Nginx service
 output "nginx_external_ip" {
   value = kubernetes_service.nginx.status[0].load_balancer[0].ingress[0].ip
+}
+
+# Output the external IP of the Grafana service
+output "grafana_external_ip" {
+  value = data.kubernetes_service.grafana.status[0].load_balancer[0].ingress[0].ip
+}
+
+# Data source to get Grafana service details
+data "kubernetes_service" "grafana" {
+  metadata {
+    name      = "grafana"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
+  depends_on = [
+    helm_release.grafana
+  ]
+}
+
+# Output the external IP of the Prometheus server
+output "prometheus_external_ip" {
+  value = data.kubernetes_service.prometheus.status[0].load_balancer[0].ingress[0].ip
+}
+
+# Data source to get Prometheus service details
+data "kubernetes_service" "prometheus" {
+  metadata {
+    name      = "prometheus-server"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
+  depends_on = [
+    helm_release.prometheus
+  ]
 }
